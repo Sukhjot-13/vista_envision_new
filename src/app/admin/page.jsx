e6cd712e349a11e4dcf7dc4ad48b51929097ac06
Sukhjot-13 <sukhjotsingh441@gmail.com>
2025-11-23 19:00:20 -0500

    added some logs

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -61,7 +61,7 @@ export default async function AdminPage() {
             {projects.length === 0 && (
                 <tr>
                     <td colSpan="3" className="px-6 py-4 text-center text-gray-500">
-                        No projects found.
+                        No projects found. If you are in production, your database might be empty.
                     </td>
                 </tr>
             )}
